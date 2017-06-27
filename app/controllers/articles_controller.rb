058b60bbf80d429d3d4ca0d1e837f7e4df426ff3
Simon Zhen <simonzhen@me.com>
2017-06-27 20:46:18 +0000

    Added basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   
+  http_basic_authenticate_with name: "sz", password: "secret", except: [:index, :show]
+  
   def index
     @articles = Article.all
   end
@@ -9,19 +11,40 @@ class ArticlesController < ApplicationController
   end
   
   def new
+    @article = Article.new
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
   end
   
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
   
+  def update 
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
   
-  private
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
   
+private
   def article_params
     params.require(:article).permit(:title, :text)
   end
-  
 end
